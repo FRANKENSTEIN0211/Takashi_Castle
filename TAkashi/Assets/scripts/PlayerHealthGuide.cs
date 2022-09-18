ba2df9b38186aed0616bc1cf282054aa328943d5
FRANKENSTEIN0211 <98984204+FRANKENSTEIN0211@users.noreply.github.com>
2022-09-18 07:54:29 +0530

    multiplayer done

---
+++ TAkashi/Assets/scripts/PlayerHealthGuide.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FishNet.Object.Synchronizing;
+using TMPro;
+
+public class PlayerHealthGuide : MonoBehaviour
+{
+    [SyncVar] public int health=10;
+    private TextMeshProUGUI healthText;
+    
+}
---
+++ TAkashi/Assets/scripts/PlayerShoot.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FishNet.Connection;
+using FishNet.Object;
+
+public class PlayerShoot : NetworkBehaviour
+{
+    public int damage;
+    public float timeBetweenfire;
+    float fireTimer;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(!base.IsOwner)
+        return;
+
+        if(Input.GetButton("Fire1"))
+        {
+            if(fireTimer<=0){
+                ShootServer(damage,Camera.main.transform.position,Camera.main.transform.forward);
+                fireTimer=timeBetweenfire;
+            }
+        }
+
+        if(fireTimer>0)
+        fireTimer-=Time.deltaTime;
+    }
+
+    [ServerRpc (RequireOwnership =false)]
+    private void ShootServer(int damageToGive,Vector3 position, Vector3 direction){
+          if(Physics.Raycast(position,direction,out RaycastHit hit) && hit.transform.TryGetComponent(out PlayerhealthGuide enemyHealth)){
+            enemyHealth.health-=damageToGive;
+          }
+    }
+}
